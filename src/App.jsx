9b4e0ba39e844da5615325478fa2e660fe10d3b7
akmalshakirov <akmalpubg221@gmail.com>
2024-03-09 20:51:01 +0500

    exam

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import menuData from "./helpers/data";
+import { Card, Modal } from "antd";
+
+const App = () => {
+    const Clicked = () => {
+        return alert("This button clicked !");
+    };
+
+    const openModal = () => {
+        <Modal>
+            <h1>Haridingiz uchun raxmat !</h1>
+        </Modal>;
+    };
+    const [comment, setComment] = useState([]);
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then((response) => response.json())
+            .then((result) => setComment(result));
+    }, []);
+
+    return (
+        <div>
+            <div className='flex flex-col items-center text-center mt-[20px]'>
+                <h1 className='text-[#DC780B] text-base'>{menuData.title}</h1>
+                <h1 className='text-[#150C01] text-3xl mb-[25px]'>
+                    {menuData.text}
+                </h1>
+                <h1 className='max-w-[748px] text-[#150C01] text-base opacity-70'>
+                    {menuData.comment}
+                </h1>
+                <div className='flex gap-[30px] flex-wrap p-[100px]'>
+                    {/* <Card> */}
+                    {/* {comment.map((item) => (
+                            <div
+                                key={item.id}
+                                className='items-center justify-center'>
+                                <div className='flex flex-col border-[1px] border-[#000] rounded items-center p-[25px] w-[360px] rounded-[40px]'>
+                                    <div>
+                                        <img
+                                            src={item.image}
+                                            alt='img'
+                                            width={100}
+                                            height={100}
+                                        />
+                                    </div>
+                                    <div className='flex'>
+                                        <h1>{item.title}</h1>
+                                        <p>{item.price}$</p>
+                                    </div>
+                                    <p className='max-w-[310px]'>
+                                        {item.category}
+                                    </p>
+                                </div>
+                            </div>
+                        ))} */}
+                    {comment.map((item) => (
+                        <div key={item.id}>
+                            <Card
+                                onClick={openModal()}
+                                hoverable
+                                style={{
+                                    width: 300,
+                                }}>
+                                <div>
+                                    <div>
+                                        <img src={item.image} alt='image' />
+                                        <div className='flex flex-col justify-between'>
+                                            <p className='mt-[10px]'>
+                                                {item.title}
+                                            </p>
+                                            <p>{item.price}</p>
+                                        </div>
+                                        <p>{item.category}</p>
+                                    </div>
+                                    <div className='flex justify-between'>
+                                        <button
+                                            onClick={Clicked}
+                                            className='text-white rounded-[5px] bg-[#DC780B] py-[11px] px-[15px]'>
+                                            +
+                                        </button>
+                                        <h1>++++</h1>
+                                    </div>
+                                </div>
+                            </Card>
+                        </div>
+                    ))}
+                    {/* </Card> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/helpers/data.jsx
@@ -0,0 +1,22 @@
+const menuData = {
+    id: 1,
+    title: "MENU",
+    text: "Food Full of treaty Love",
+    comment:
+        "There are many things are needed to start the Fast Food Business. You need not only Just Food Stalls with Persons but also specialized equipment, Skills to manage Customers, ",
+    star: (props) => (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width={16}
+            height={15.402}
+            fill='none'
+            {...props}>
+            <path
+                fill='#DC780B'
+                d='M16 5.883 10.186 5.5 7.996 0 5.808 5.5 0 5.883l4.454 3.782-1.461 5.737 5.004-3.163L13 15.402 11.54 9.665 16 5.883Z'
+            />
+        </svg>
+    ),
+};
+
+export default menuData;
